54cb817a0d000181484f1efe9efb8eb470b6cd5c
Slanecznik <vova.shigaev121183@gmail.com>
2026-05-14 00:47:04 +0200

    1.2

--- script.js
+++ script.js
@@ -10,10 +10,38 @@ const model = {
 
 // отображение данных: рендер списка задач, размещение обработчиков событий
 const view = {
-    renderTasks(tasks) {
+    init() {
+        this.renderTasks(model.tasks)
+    },
 
-    }
+    renderTasks(tasks) {
+        const list = document.querySelector('.list')
+
+        let tasksHTML = ''
+
+        for (let i = 0; i < tasks.length; i++) {
+            const task = tasks[i]
+
+            tasksHTML += `
+        <li id="${task.id}" class="${task.isDone ? 'done' : ''}">
+          <b class="task-title">${task.title}</b>
+          <button class="delete-button" type="button">Удалить 🗑</button>
+        </li>
+      `
+        }
+
+        list.innerHTML = tasksHTML
+    },
 }
+
+    // Функция инициализации
+    function init() {
+    view.init()
+    // здесь может быть код инициализации других модулей
+}
+
+// Вызов функции инициализации при загрузке страницы
+init()
 
 // обработка действий пользователя, обновление модели
 const controller = {}
