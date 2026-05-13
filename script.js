5eaae5e7295e717599deddbf9189b145c110f0dc
Slanecznik <vova.shigaev121183@gmail.com>
2026-05-14 00:22:01 +0200

    1

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+const MOCK_TASKS = [
+    { id: 1, title: 'Изучить паттерн MVC', isDone: false },
+    { id: 2, title: 'Подготовить моковые данные', isDone: true },
+]
+
 // хранение данных, бизнес-логика
-const model = {}
+const model = {
+    tasks: MOCK_TASKS
+}
 
 // отображение данных: рендер списка задач, размещение обработчиков событий
-const view = {}
+const view = {
+    renderTasks(tasks) {
+
+    }
+}
 
 // обработка действий пользователя, обновление модели
 const controller = {}
